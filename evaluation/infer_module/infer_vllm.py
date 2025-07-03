fc7541f1a614eacbeefee7c67159014bb9e3031a
1421974407 <jp2022213320@qmul.ac.uk>
2025-07-03 16:35:35 +0800

    Update infer_vllm.py

--- evaluation/infer_module/infer_vllm.py
+++ evaluation/infer_module/infer_vllm.py
@@ -125,7 +125,7 @@ class LLM_Service:
     def get_response_tokenIDs(self, request_results: List) -> List[List]:
         token_ids = [
             [
-                result.token_ids[0]
+                list(result.token_ids)
                 for result in request_result.outputs
             ]
             for request_result in request_results
@@ -136,7 +136,7 @@ class LLM_Service:
     def get_response_tokens(self, request_results: List) -> List[List]:
         tokens = [
             [
-                self.tokenizer.decode(result.token_ids[0])
+                self.tokenizer.decode(list(result.token_ids))
                 for result in request_result.outputs
             ]
             for request_result in request_results
